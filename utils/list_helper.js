93659a5359938f7310903ffba8fa90ef32c2257d
Hassan Nurudeen <nurudeenhassan862@gmail.com>
2024-08-26 21:58:59 +0100

    feat: updated to 4.7* solution

---
+++ utils/list_helper.js
@@ -0,0 +1,61 @@
+const _ = require('lodash')
+
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  return blogs.reduce((favorite, blog) => {
+    return blog.likes > favorite.likes ? blog : favorite
+  })
+
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const blogCounts = _.countBy(blogs, 'author')
+
+  const authorsWithBlogs = _.map(blogCounts, (blogs, author) => ({ author, blogs }))
+
+  const topAuthor = _.maxBy(authorsWithBlogs, 'blogs')
+
+  return topAuthor
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const likesByAuthor = _.groupBy(blogs, 'author')
+
+  const totalNoLikes = _.map(likesByAuthor, (authorBlogs, author) => ({
+    author,
+    likes: _.sumBy(authorBlogs, 'likes')
+  }))
+
+  const topAuthor = _.maxBy(totalNoLikes, 'likes')
+
+  return topAuthor
+
+}
+
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
